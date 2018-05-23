521633892fd85976b3f68cccf4b57f36af10d3ae
Tam <luongtam1101995>
2018-05-23 16:35:27 +0700

    Fix error cannot load Helper

--- Controller/Adminhtml/Order/Delete.php
+++ Controller/Adminhtml/Order/Delete.php
@@ -1,5 +1,23 @@
 <?php
-
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_DeleteOrder
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     http://mageplaza.com/LICENSE.txt
+ */
 namespace Mageplaza\DeleteOrder\Controller\Adminhtml\Order;
 
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection as DbAC;
