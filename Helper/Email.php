b97e7670215650122fcd8108c37f85f0c1520159
vinhlt <vinhlt@mageplaza.com>
2020-02-05 17:47:13 +0700

    Clean code 2

--- Helper/Email.php
+++ Helper/Email.php
@@ -52,7 +52,6 @@ class Email extends AbstractData
 
     /**
      * Email constructor.
-     *
      * @param Context $context
      * @param ObjectManagerInterface $objectManager
      * @param StoreManagerInterface $storeManager
@@ -65,7 +64,8 @@ class Email extends AbstractData
         StoreManagerInterface $storeManager,
         TransportBuilder $transportBuilder,
         HelperData $helperData
-    ) {
+    )
+    {
         $this->transportBuilder = $transportBuilder;
         $this->_helperData = $helperData;
 
@@ -75,7 +75,6 @@ class Email extends AbstractData
     /**
      * @param array $templateParams
      * @param null $storeId
-     *
      * @return $this
      */
     public function sendEmailTemplate($templateParams = [], $storeId = null)
